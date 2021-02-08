476ec414e0b61451d954948da2cdd7c084bd0aa6
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-02-08 16:29:17 -0500

    Add Source Select

--- src/render.js
+++ src/render.js
@@ -9,8 +9,19 @@ videoSelectBtn.onClick = getVideoSources;
 //Get all screens on desktop
 // desktopCapturer records screen, remote access' IPC (host proceses)
 const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
 const getVideoSources = async () => {
   const inputSources = await desktopCapturer.getSources({
     types: ['window', 'screen']
   });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source),
+      };
+    })
+  );
+  videoOptionsMenu.popup();
 };
